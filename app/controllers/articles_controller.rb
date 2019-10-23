2a54b5608091bc373cb507766a72632ffe65476c
sacha mestdagh <sacha.mestdagh@hotmail.com>
2019-10-23 08:27:23 +0800

    wikinews start

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,28 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def set_article
+  end
+
+  def article_params
+  end
+end
